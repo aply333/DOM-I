88de5d53295a2c8c396fb7b728e4d5eb62843947
Andrei Lysenko <andrei.lysenko@outlook.com>
2020-01-15 20:21:16 -0800

    struggling, but slowly getting it done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,46 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navItems = document.querySelectorAll('a');
+  navItems.forEach(el => {
+  navItems.textContent = siteContent["nav"][el]
+})
+
+let ctaParent = document.querySelector(".cta");
+  ctaParent.querySelector('h1').textContent = siteContent["cta"]["h1"]
+  ctaParent.querySelector('button').textContent = siteContent["cta"]["button"]
+  ctaParent.querySelector('img').setAttribute('src', siteContent["cta"]["img-src"])
+
+let mainTextContent = document.querySelector(".main-content").querySelectorAll(".text-content")
+    mainTextContent[0].querySelector('h4').textContent = siteContent["main-content"]["features-h4"]
+    mainTextContent[0].querySelector('p').textContent = siteContent["main-content"]["features-content"]
+    mainTextContent[1].querySelector('h4').textContent = siteContent["main-content"]["about-h4"]
+    mainTextContent[1].querySelector('p').textContent = siteContent["main-content"]["about-content"]
+    mainTextContent[2].querySelector('h4').textContent = siteContent["main-content"]["services-h4"]
+    mainTextContent[2].querySelector('p').textContent = siteContent["main-content"]["services-content"]
+    mainTextContent[3].querySelector('h4').textContent = siteContent["main-content"]["product-h4"]
+    mainTextContent[3].querySelector('p').textContent = siteContent["main-content"]["product-content"]
+    mainTextContent[4].querySelector('h4').textContent = siteContent["main-content"]["vision-h4"]
+    mainTextContent[4].querySelector('p').textContent = siteContent["main-content"]["vision-content"]
+
+document.querySelector(".main-content").querySelector("img").setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+// document.querySelector(".contact").querySelector("h4").textContent = siteContent["contact"]["contact-h4"]
+
+let contactSection = document.querySelector(".contact")
+let contactInfo = siteContent["contact"]
+
+contactSection.forEach(el =>{
+  console.log(el, "testing")
+})
+
+document.querySelector("footer").querySelector("p").textContent = siteContent["footer"]["copyright"]
+      // let ctaH1 = document.querySelectorAll('h1');
+// ctaH1.text = siteContent["cta"]['h1']
+
+// let ctaButton = document.querySelectorAll('button');
+// ctaButton.textContent = siteContent["cta"][["button"]]
+
+// console.log('img-cta',ctaImg)
+// ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
